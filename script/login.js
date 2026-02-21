66496a540414e4e7d778004c4501611aaaffb651
mehedi3322 <mehedihasan88990011@gmail.com>
2026-02-21 23:45:08 +0600

    completed login page functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,19 @@
-console.log("login page is landing--");
+// console.log("login page is landing--");
+document.getElementById("login-btn")
+.addEventListener("click",function(){
+    const mobileNumber = document.getElementById("mobile-number");
+    const number=mobileNumber.value;
+    // console.log(number);
+    const pinNumber = document.getElementById("pin-number");
+    const pin = pinNumber.value;
+    if(number==="01877948007" && pin==="1234")
+    {
+        alert("login successfull");
+        window.location.assign("/home.html")
+    }
+    else
+    {
+        alert("Try again");
+        return;
+    }
+})
